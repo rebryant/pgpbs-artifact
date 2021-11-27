775214f43dd4b1060111d040ac517de3594b9f1b
Randal E. Bryant <Randy.Bryant@cs.cmu.edu>
2021-11-27 15:59:00 -0500

    Execution script

---
+++ experimental-results/reproduced-data/fig6/gen-short.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+make bcr N=004
+make bcr N=008
+make bcr N=012
+make tcs N=004
+make tcs N=008
+make tcs N=012
+make tcs N=016
+make ter N=004
+make ter N=008
+make ter N=012
+make ter N=016
+make ter N=024
+make ter N=032
+make ber N=004
+make ber N=008
+make ber N=012
+make ber N=016
+make ber N=024
+make ber N=032
